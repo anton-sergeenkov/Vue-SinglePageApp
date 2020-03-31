7a2893b00e74ce71ff1e0a4d079571e3a7b2bc11
Anton <warnes23@yandex.ru>
2020-03-31 12:47:03 +0300

    PageArticle api

---
+++ src/api/articleService.js
@@ -0,0 +1,3 @@
+const URL_GET_ARTICLE = 'https://api.myjson.com/bins/ja5kk';
+
+export const fetchArticle = () => fetch(URL_GET_ARTICLE);
--- src/components/PageArticle.vue
+++ src/components/PageArticle.vue
@@ -1,8 +1,30 @@
 <template>
     <div class="wrapper">
-        Article
+        <h2>{{articleHeader}}</h2>
+        <p>{{articleContent}}</p>
     </div>
 </template>
+
+<script>
+import { fetchArticle } from '../api/articleService';
+
+export default {
+    data() {
+        return {
+            articleHeader: null,
+            articleContent: null,
+        };
+    },
+    created() {
+        fetchArticle()
+            .then(res => res.json())
+            .then(({ header, content }) => {
+                this.articleHeader = header;
+                this.articleContent = content;
+            });
+    }
+}
+</script>
 
 <style scoped>
 
