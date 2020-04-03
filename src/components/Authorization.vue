7b88bcc34ccdee8e7f3520feda0dc704d84062b7
Anton <warnes23@yandex.ru>
2020-04-03 13:21:11 +0300

    uikit uiDialog

--- src/components/Authorization.vue
+++ src/components/Authorization.vue
@@ -14,7 +14,27 @@
                 theme="flat"
             />
         </div>
+
         <strong v-else>Имя пользователя</strong>
+
+        <ui-dialog v-if="openModal" @close="closeModal">
+            <template v-slot:header>Вход на сайт</template>
+            <template v-slot:form>
+                <form @submit.prevent="checkForm" class="wrapper-form">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Expedita architecto impedit a quam ad recusandae repellendus 
+                        voluptatem amet quo, dolore sunt id incidunt asperiores omnis 
+                        alias doloribus inventore. Iusto, laborum
+                    </p>
+                    <div class="wrapper-form-btn">
+                        <ui-button class="form-btn" label="Отмена" @click.native.prevent="closeModal" />
+                        <ui-button class="form-btn" label="Ок" theme="primary" />
+                    </div>
+                </form>
+            </template>
+        </ui-dialog>
+
     </div>
 </template>
 
@@ -26,15 +46,22 @@ export default {
         return {
             userAuthorized: false,
             users: null,
+            openModal: false
         };
     },
     methods: {
+        closeModal() {
+            this.openModal = false;
+        },
+        checkForm() {
+			alert('ok');
+        },
         handlerAuthorization() {
-            alert('handlerAuthorization');
+            this.openModal = true;
         },
         handlerRegistration() {
             alert('handlerRegistration');
-        },
+        }
     },
     created() {
         fetchUsers()
@@ -51,5 +78,23 @@ export default {
 }
 .btn {
     margin: 0 5px;
+}
+.wrapper-form {
+    width: 400px;
+    line-height: 1.5;
+    padding: 20px;
+}
+.wrapper-form p {
+    margin-bottom: 30px;
+}
+
+.wrapper-form-btn {
+    display: flex;
+    justify-content: center;
+    margin-top: 45px;
+}
+.form-btn {
+    margin: 0 5px;
+    min-width: 150px;
 }
 </style>
--- src/uikit/uiDialog.vue
+++ src/uikit/uiDialog.vue
@@ -43,23 +43,13 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
-// $indigo:     #3f51b5;
-// $blue:       #005caf;
-// $blue-light: #2196f3;
-// $green:      #4caf50;
-// $red:        #f44336;
-// $orange:     #ff9800;
-// $silver:     #c9c5c5;
-
-// $gray:       #727272;
-// $light:      #f5f5f5;
-
-
-$color-wrapper: rgba(0,0,0,0.3);
-$color-form: #fff;
-$color-form-header: #dddcdc;
-$fz-header: 20px;
+<style scoped>
+.component-ui-modal-dialog {
+    --color-wrapper: rgba(0, 0, 0, 0.3);
+    --color-form: #fff;
+    --color-form-header: #dddcdc;
+    --fz-header: 20px;
+}
 
 .component-ui-modal-dialog {
     display: flex;
@@ -71,25 +61,25 @@ $fz-header: 20px;
     top: 0;
     left: 0;
     z-index: 100;
-    background-color: $color-wrapper;
+    background-color: var(--color-wrapper);
 }
 .modal-dialog {
     position: relative;
     border-radius: 2px;
-    background-color: $color-form;
-    box-shadow: 0px 5px 5px 0px $color-wrapper;
-    .header {
-        padding: 20px 30px;
-        background: $color-form-header;
-        font-size: $fz-header;
-        color: rgba(0,0,0,0.65);
-    }
-    .form {
-        padding: 20px;
-    }
+    background-color: var(--color-form);
+    box-shadow: 0px 5px 5px 0px var(--color-wrapper);
+}
+.modal-dialog .header {
+    padding: 20px 30px;
+    background: var(--color-form-header);
+    font-size: var(--fz-header);
+    color: rgba(0, 0, 0, 0.65);
+}
+.modal-dialog .form{
+    padding: 20px;
 }
 
-// transition
+/* transition */
 .fade-enter-active, .fade-leave-active {
     transition: opacity .4s;
 }
